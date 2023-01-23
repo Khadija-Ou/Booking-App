d2b6db59329d70d919e9e9d380264f935eb94dea
Khadija <khadija@Khadijas-MacBook-Pro.local>
2023-01-23 14:14:25 +0100

    header changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -88,7 +88,9 @@ const Header = ({ type }) => {
               Get rewarded for your travels – unlock instant savings
               of 10% or more with a free Lamabooking account
             </p>
-            <button className='headerBtn'>Sign in / Register</button>
+            <button className='headerButton'>
+              Sign in / Register
+            </button>
             <div className='headerSearch'>
               <div className='headerSearchItem'>
                 <FontAwesomeIcon
